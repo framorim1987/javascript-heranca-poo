d0bcecbcc0a4bbf584da3bf9a8d645f5cc12c8fa
framorim1987 <framorim1987@gmail.com>
2020-06-29 22:31:19 -0300

    Conhecendo o problema do cliente

---
+++ Conta.js
@@ -0,0 +1,29 @@
+export class Conta {
+    constructor(saldoiInicial, cliente, agencia) {
+
+        this._agencia = agencia;
+        this._cliente = cliente;
+        this._saldo = saldoiInicial;
+    }
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) {
+            return;
+        }
+        this._saldo += valor;
+    }
+
+    tranferir(valor, conta) {
+
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+
+    }
+}
